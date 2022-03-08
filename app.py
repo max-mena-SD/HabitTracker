09de880d937612671881f29baaf75ebb66e1314b
Max Mena <raemxam@gmail.com>
2022-03-08 17:53:45 -0600

    avance en la casa de dona Marcela

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+habits= ["test_habits"]
 
 app= Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Habit Tracker - Home")
+    return render_template("index.html",habits=habits , title="Habit Tracker - Home")
 
 @app.route("/add", methods=["GET", "POST"])
 def add_habit():
+    if request.form == "POST":
+        pass
+
     return render_template("add_habit.html", title="Habit Tracker - Add Habit")
