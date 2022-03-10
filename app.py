ebb293c5887df36f16aa47040659d52e0de80224
maxmenaarguedas <raemxam@gmail.com>
2022-03-09 18:03:57 -0600

    respaldo porque Flask esta malfuncionando

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template, request
+import datetime
+from flask import Flask, render_template, request, request_started
 
 habits= []
 
 app= Flask(__name__)
 
+def date_range(start: datetime.date):
+    dates= [start + datetime.timedelta(days=day) for day in range(-3,4)]
+    return dates
+
 @app.route("/")
 def index():
-    return render_template("index.html",habits=habits , title="Habit Tracker - Home")
+
+    date_str= request.args.get("date")
+
+    if date_str:
+        selected_date= datetime.date.fromisoformat(date_str)
+    else:
+        selected_date=datetime.date.today()
+
+    return render_template(
+        "index.html", habits=habits , title="Habit Tracker - Home", date_range=date_range, selected_date=selected_date
+        )
 
 @app.route("/add", methods=["GET", "POST"])
 def add_habit():
