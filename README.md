b2ec60a971e3758afbb4004ec256b7416ec3fc28
Max Mena <raemxam@gmail.com>
2022-03-28 23:12:59 -0600

    Pagina funcionando pero el problema es que la semana no se encuentra, problema con la fecha

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # HabitTracker
 
 Como parte de continuar practicando Python y poner en pactica dicho aprendizaje desarrollo esta aplicacion sencilla.
+
+
+in order to run flask in a virtual enviroment:
+primero necesito crear el folder con el ambiente virtual:
+    python3 -m venv .venv
+
+luego para inciar el uso del ambiente virtual se ejecuta
+    . .venv/bin/activate (para Linux/Mac)
+    . .venv/Scripts/activate (para Windows)
+
+por ultimo para que Flask sepa cual es el proyecto que estamos ejecutando, se actualizan las siguientes variables, la segunda no es obligatoria es para aclarar que se encuentra en desarrollo.
+    export FLASK_APP=app.py
+    export FLASK_ENV=development
+
+A este punto el ambiente virtual probablemente no tenga instalado ninguna de las bibliotecas necesarias para correr el programa, por lo que deberia ejecutarse el archivo requirements.txt:
+    pip install requirements.txt
--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import datetime
-from flask import Flask, render_template, request, request_started
+from collections import defaultdict
+from flask import Flask, render_template, request, request_started, redirect, url_for
 
-habits= []
+app = Flask(__name__)
+habits = ["test habits"]
+completions = defaultdict(list)
 
-app= Flask(__name__)
-
-def date_range(start: datetime.date):
-    dates= [start + datetime.timedelta(days=day) for day in range(-3,4)]
-    return dates
+@app.context_processor
+def add_calc_date_range():
+    def date_range(start: datetime.date):
+        dates= [start + datetime.timedelta(days=day) for day in range(-3,4)]
+        return dates
+    return {"date_range": date_range}
 
 @app.route("/")
 def index():
@@ -20,7 +24,11 @@ def index():
         selected_date=datetime.date.today()
 
     return render_template(
-        "index.html", habits=habits , title="Habit Tracker - Home", date_range=date_range, selected_date=selected_date
+        "index.html",
+         habits=habits,
+         selected_date=selected_date,
+         completions=completions[selected_date],
+         title="Habit Tracker - Home",  
         )
 
 @app.route("/add", methods=["GET", "POST"])
@@ -29,4 +37,17 @@ def add_habit():
         habit=request.form.get("habit")
         habits.append(habit)
 
-    return render_template("add_habit.html", title="Habit Tracker - Add Habit")
+    return render_template(
+        "add_habit.html", 
+        title="Habit Tracker - Add Habit", 
+        selected_date= datetime.date.today()
+    )
+
+@app.route("/complete", methods=["POST"])
+def complete():
+    date_string = request.form.get("date")
+    habit = request.form.get("habitName")
+    date = datetime.date.fromisoformat(date_string)
+    completions[date].append(habit)
+
+    return redirect(url_for("index", date=date_string))
